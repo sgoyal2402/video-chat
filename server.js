f44eaa4bf270e1c2045ef2179a08ec7e53248995
Suryansh <sgoyal2402@gmail.com>
2021-06-16 15:42:53 +0530

    new

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Set public folder as root
 app.use(express.static('public'));
@@ -12,7 +12,7 @@ app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-app.listen(process. env. PORT || port, () => {
+app.listen( port, () => {
   // eslint-disable-next-line no-console
   console.info('listening on %d', port);
 });
